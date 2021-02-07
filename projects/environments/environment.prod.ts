4bce0b4c7f2247bf9ebbcd978a3523c5df81015e
JonAlco <alco_1993@hotmail.com>
2021-02-07 00:47:01 -0500

    [actualizacione] - TOKEN_API - API en Azure

--- projects/environments/environment.prod.ts
+++ projects/environments/environment.prod.ts
@@ -8,8 +8,15 @@ export const environment = {
   PATH_ASSETS_IMAGES_MENU_LOGO: '/assets/images/logo-min.png',
   PATH_ASSETS_IMAGES_MENU: '/assets/images/menu/',
 
-  SOCKET_ENDPOINT: 'https://app-manhattan-api.herokuapp.com:3456/',
-  BACKEND_ENDPOINT: 'https://app-manhattan-api.herokuapp.com/api/',
+  TOKEN_API: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoyLCJ1c2VybmFtZSI6Im1hbmhhdHRhbiIsImVtYWlsIjoiamFsY29jZXJ6YW1vcmFAZ21haWwuY29tIn0sImlhdCI6MTYwNjcwMDk3OX0.NYyJyrC0riLa7fEBrnh1wBidFQxeZ01CsljEbyPDheY',
+
+  // HEROKU
+  // SOCKET_ENDPOINT: 'https://app-manhattan-api.herokuapp.com:3456/',
+  // BACKEND_ENDPOINT: 'https://app-manhattan-api.herokuapp.com/api/',
+
+  // AZURE
+  SOCKET_ENDPOINT: 'https://api-manhattan.azurewebsites.net:3456/',
+  BACKEND_ENDPOINT: 'https://api-manhattan.azurewebsites.net/v1/',
 
   BACKEND_USERNAME: 'manhattan',
   BACKEND_PASSWORD: 'manhattan-prod',
