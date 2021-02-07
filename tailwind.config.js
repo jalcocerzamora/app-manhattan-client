044999b7d4b4926203941d9fbbbd7a544330c289
JonAlco <alco_1993@hotmail.com>
2021-02-07 00:42:25 -0500

    [actualizacion] - packages.json - tailwindcss.config.js - webpack.config.js:  + leaflet   + postcss-tailwindcss
    [removio]
    - tsconfig.json:
     + emitDecoratorMetadata
     + forceConsistentCasingInFileNames

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const plugin = require('tailwindcss/plugin')
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   important: false,
@@ -6,7 +7,97 @@ module.exports = {
   //   enabled: true,
   //   content: './projects/**/*.html'
   // },
+  // purge: ['./src/**/*.html', './src/**/*.ts'],
   theme: {
+    fontFamily: {
+      display: ['Poppins', 'system-ui', 'sans-serif'],
+      body: ['Poppins', 'system-ui', 'sans-serif'],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: '#000',
+      white: '#fff',
+      gray: colors.trueGray,
+      red: colors.red,
+      blue: colors.blue,
+      yellow: colors.amber,
+      orange: colors.orange,
+      green: colors.green,
+    },
+    customForms: theme => ({
+      default: {
+        'input, textarea, multiselect, select': {
+          fontSize: theme('fontSize.xs'),
+          padding: `${theme('spacing.1')} ${theme('spacing.2')}`,
+        },
+        input: {
+          borderRadius: theme('borderRadius.lg'),
+          borderColor: theme('colors.gray.100'),
+          boxShadow: theme('boxShadow.md'),
+          // backgroundColor: theme('colors.white'),
+          // '&:focus': {
+          //   backgroundColor: theme('colors.white'),
+          // }
+        },
+        select: {
+          icon: '<svg fill="#e2e8f0" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>',
+          borderRadius: theme('borderRadius.lg'),
+          boxShadow: theme('boxShadow.default'),
+        },
+        checkbox: {
+          icon: '<svg fill="#1a202c" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" ><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>',
+          '&:hover': {
+            icon: '<svg fill="#4a5568" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" ><path d="M0 11l2-2 5 5L18 3l2 2L7 18z"/></svg>',
+          },
+          width: theme('spacing.6'),
+          height: theme('spacing.6'),
+        },
+        radio: {
+          icon: '<svg fill="#fff" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><circle cx="8" cy="8" r="3"/></svg>'
+        },
+        textarea: {
+          width: theme('width.full'),
+          backgroundColor: theme('colors.white'),
+          borderColor: theme('colors.gray.300'),
+          borderRadius: theme('borderRadius.md'),
+        }
+      },
+      // dark: {
+      //   'input, textarea, multiselect, checkbox, radio': {
+      //     backgroundColor: theme('colors.gray.900'),
+      //   },
+      //   select: {
+      //     backgroundColor: theme('colors.gray.600'),
+      //   },
+      // },
+      xs: {
+        'input, textarea, multiselect, select': {
+          fontSize: theme('fontSize.xs'),
+          padding: `${theme('spacing.1')} ${theme('spacing.2')}`,
+        },
+        select: {
+          paddingRight: `${theme('spacing.4')}`,
+        },
+        'checkbox, radio': {
+          width: theme('spacing.3'),
+          height: theme('spacing.3'),
+        },
+      },
+      sm: {
+        // 'input, textarea, multiselect, select': {
+        //   fontSize: theme('fontSize.xs'),
+        //   padding: `${theme('spacing.1')} ${theme('spacing.2')}`,
+        // },
+        // select: {
+        //   paddingRight: `${theme('spacing.4')}`,
+        // },
+        // 'checkbox, radio': {
+        //   width: theme('spacing.3'),
+        //   height: theme('spacing.3'),
+        // },
+      }
+    }),
     extend: {
       spacing: {
         '28': '7.5rem',
@@ -14,6 +105,9 @@ module.exports = {
       // width: theme => ({ 
       //   ...theme('spacing'),
       // }),
+      colors: {
+        'regal-blue': '#243c5a',
+      },
       minWidth: {
         'xxs': '18rem',
       },
@@ -55,8 +149,15 @@ module.exports = {
     // opacity: ['responsive', 'hover']
     textTransform: ['responsive', 'firstletter'],
     pointerEvents: ['responsive', 'hover', 'focus'],
+    extend: {
+      backgroundColor: ['checked'],
+    }
   },
   plugins: [
+    require('@tailwindcss/forms'),
+
+    require('@tailwindcss/custom-forms'),
+
     plugin(function({ addVariant, e }) {
       // addVariant('scrollSnap', ({ modifySelectors, separator }) => {
       //   modifySelectors(({ className }) => {
@@ -109,67 +210,67 @@ module.exports = {
           fontWeight: '600',
         },
 
-        '.form-textarea': {
-          appearance: config('variants.appearance.none'),
-          backgroundColor: config('theme.colors.white'),
-          borderColor: config('theme.colors.gray.300'),
-          borderWidth: config('theme.borderWidth.default'),
-          borderRadius: config('theme.spacing.1'),
-          padding: `${config('theme.spacing.1')} ${config('theme.spacing.0')}`,
-          fontSize: config('theme.fontSize.xs'),
-          lineHeight: config('theme.lineHeight.none'),
+        // '.form-textarea': {
+        //   appearance: config('variants.appearance.none'),
+        //   backgroundColor: config('theme.colors.white'),
+        //   borderColor: config('theme.colors.gray.300'),
+        //   borderWidth: config('theme.borderWidth.default'),
+        //   borderRadius: config('theme.spacing.1'),
+        //   padding: `${config('theme.spacing.1')} ${config('theme.spacing.0')}`,
+        //   fontSize: config('theme.fontSize.xs'),
+        //   lineHeight: config('theme.lineHeight.none'),
 
-          '&::-moz-placeholder': {
-            color: config('theme.colors.gray.500'),
-            opacity: config('theme.opacity.100'),
-          },
+        //   '&::-moz-placeholder': {
+        //     color: config('theme.colors.gray.500'),
+        //     opacity: config('theme.opacity.100'),
+        //   },
 
-          '&:-ms-input-placeholder': {
-            color: config('theme.colors.gray.500'),
-            opacity: config('theme.opacity.100'),
-          },
+        //   '&:-ms-input-placeholder': {
+        //     color: config('theme.colors.gray.500'),
+        //     opacity: config('theme.opacity.100'),
+        //   },
 
-          '&::placeholder': {
-            color: config('theme.colors.gray.500'),
-            opacity: config('theme.opacity.100'),
-          },
+        //   '&::placeholder': {
+        //     color: config('theme.colors.gray.500'),
+        //     opacity: config('theme.opacity.100'),
+        //   },
               
-          '&:focus': {
-            outline: 'none',
-            borderColor: config('theme.colors.orange.500'),
-          }
-        },
+        //   '&:focus': {
+        //     outline: 'none',
+        //     borderColor: config('theme.colors.orange.500'),
+        //   }
+        // },
 
-        '.form-control': {
-          fontSize: config('theme.fontSize.lg'),
-          appearance: config('variants.appearance.none'),
-          outline: config('variants.outline.none'),
-          color: config('theme.colors.gray.700'),
-          boxShadow: config('theme.boxShadow.default'),
-          borderWidth: config('theme.borderWidth.default'),
-          borderRadius: config('theme.borderRadius.default'),
-          width: config('theme.width.full'),
-          padding: `${config('theme.spacing.2')} ${config('theme.spacing.3')}`,
-          // marginBottom: config('theme.margin.4'),
-          lineHeight: config('theme.lineHeight.tight'),
+        // '.form-control': {
+        //   fontSize: config('theme.fontSize.lg'),
+        //   appearance: config('variants.appearance.none'),
+        //   outline: config('variants.outline.none'),
+        //   color: config('theme.colors.gray.700'),
+        //   boxShadow: config('theme.boxShadow.default'),
+        //   borderWidth: config('theme.borderWidth.default'),
+        //   borderRadius: config('theme.borderRadius.default'),
+        //   width: config('theme.width.full'),
+        //   padding: `${config('theme.spacing.2')} ${config('theme.spacing.3')}`,
+        //   // marginBottom: config('theme.margin.4'),
+        //   lineHeight: config('theme.lineHeight.tight'),
           
-          '&:focus': {
-            outline: 'transparent 0',
-            boxShadow: config('theme.boxShadow.outline'),
-            backgroundColor: config('theme.colors.white'),
-            borderColor: config('theme.borderColor.teal.300'),
-            boxShadow: config('theme.boxShadow.outline'),
-          },
+        //   '&:focus': {
+        //     outline: 'transparent 0',
+        //     boxShadow: config('theme.boxShadow.outline'),
+        //     backgroundColor: config('theme.colors.white'),
+        //     borderColor: config('theme.borderColor.teal.300'),
+        //     boxShadow: config('theme.boxShadow.outline'),
+        //   },
 
-          '&.form-control-sm': {
-            fontSize: config('theme.fontSize.xs'),
-            height: config('theme.height.sm'),
-            padding: `${config('theme.spacing.1')} ${config('theme.spacing.1')}`,
-            // lineHeight: config('theme.lineHeight.none'),
-            borderRadius: config('theme.borderRadius.default'),
-          },
+        //   '&.form-control-sm': {
+        //     fontSize: config('theme.fontSize.xs'),
+        //     height: config('theme.height.sm'),
+        //     padding: `${config('theme.spacing.1')} ${config('theme.spacing.1')}`,
+        //     // lineHeight: config('theme.lineHeight.none'),
+        //     borderRadius: config('theme.borderRadius.default'),
+        //   },
 
-        },
+        // },
 
         '.h-80': {
           height: 'calc(100vh - 2rem)',
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,177 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+// const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const LinkTypePlugin = require('html-webpack-link-type-plugin').HtmlWebpackLinkTypePlugin;
+// const CopyPlugin = require('copy-webpack-plugin');
+
+// import './node_modules/leaflet/dist/leaflet.css';
+
+// For pure CSS - /\.css$/i,
+// For Sass/SCSS - /\.((c|sa|sc)ss)$/i,
+// For Less - /\.((c|le)ss)$/i,
+
 module.exports = {
+    // externals: {
+    //     moment: 'moment'
+    // },
+    devServer: {
+        contentBase: './',
+        watchContentBase: true
+    },
+    resolve: {
+        modules: [
+        // path.join(__dirname, "src"),
+        'node_modules',
+        ],
+        alias: {
+            'chart.js': 'chart.js',
+            'mapbox-gl': 'mapbox-gl',
+            'marker-shadow.png': path.resolve(__dirname, '/app-manhattan-client/projects/store/src/assets/images/leaftlet/marker-shadow.png'),
+            // 'node_modules/leaflet/dist/leaflet.css': path.resolve(__dirname, '/node_modules/leaflet/dist/leaflet.css')
+            // 'leaflet': path.resolve(__dirname, '/node_modules/leaflet/dist/leaflet.css')
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: './node_modules/leaflet/dist/leaflet.css',
+        }),
+        // new CopyPlugin({
+        //     patterns: [
+        //       { from: "assets", to: "assets" },
+        //     ],
+        // }),
+        // new HtmlWebpackPlugin({
+        //     template: 'index.html'
+        // }),
+        // new LinkTypePlugin({
+        //     '**/*.css' : 'text/css'
+        // }),
+    ],
     module: {
         rules: [
+            // { test: /\.css$/i, loaders: [ 'style-loader', 'css-loader' ] },
             {
-                test: /\.scss$/,
-                loader: 'postcss-loader',
-                options: {
-                    ident: 'postcss',
-                    syntax: 'postcss-scss',
-                    plugins: () => [
-                        require('postcss-import'),
-                        require('tailwindcss'),
-                        require('autoprefixer'),
-                        // require('bootstrap'),
-                    ]
-                }
-            },
-            {
-                test: /\.(png|jpg|gif)$/,
+                test: /\.((c|sa|sc)ss)$/i,
                 use: [
+                    // "style-loader",
                     {
-                        loader: 'file-loader',
+                        loader: "postcss-loader",
                         options: {
-                            outputPath: 'assets/images/',
-                            publicPath: 'assets/images/',
+                            // plugins: () => [postcssPresetEnv({ stage: 0 })],
+                            // importLoaders: 1,
+                            postcssOptions: {
+                                ident: 'postcss',
+                                syntax: 'postcss-scss',
+                                plugins: [
+                                    require('postcss-import'),
+                                    require('tailwindcss'),
+                                    require('autoprefixer'),
+                                ],
+                            },
+                        },
+                    },
+                    // { loader: "sass-loader", },
+                ],
+            },            
+            {
+                test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+                loader: "url-loader",
+                options: {
+                    limit: 8192,
+                },
+            },
+            /*
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",                  
+                    {
+                        loader: "css-loader",
+                        options: {
+                            // Run `postcss-loader` on each CSS `@import`, do not forget that `sass-loader` compile non CSS `@import`'s into a single file
+                            // If you need run `sass-loader` and `postcss-loader` on each CSS `@import` please set it to `2`
+                            importLoaders: 2,
+
+                            // Automatically enable css modules for files satisfying `/\.module\.\w+$/i` RegExp.
+                            modules: { auto: true },
                         },
                     },
                 ],
+            },*/
+            /*
+            {
+                test: /\.(eot|svg|ttf|woff|woff2)$/,
+                loader: 'file-loader',
+                options: {
+                    publicPath: "./",
+                    outputPath: "app"
+                }
             },
+            */
+            // {
+            //     test: /\.(png|jpg|gif)$/,
+            //     use: [
+            //         {
+            //             loader: 'file-loader',
+            //             options: {
+            //                 outputPath: 'assets/images/',
+            //                 publicPath: 'assets/images/',
+            //             },
+            //         },
+            //     ],
+            // },
+
+            // {
+            //     test: /\.s[ac]ss$/i,
+            //     // // test: /\.scss$/,
+            //     loader: 'postcss-loader',
+            //     options: {
+            //         postcssOptions: {
+            //             ident: 'postcss',
+            //             syntax: 'postcss-scss',
+            //             plugins: [
+            //               require('postcss-import'),
+            //               require('tailwindcss'),
+            //               require('autoprefixer'),
+            //             ],
+            //         },
+            //     }
+            // },
+            // {
+            //     test: /\.css$/,
+            //     exclude: /node_modules/,
+            //     use: [
+            //       {
+            //         loader: 'style-loader',
+            //       },
+            //       {
+            //         loader: 'css-loader',
+            //         options: {
+            //           importLoaders: 1,
+            //         }
+            //       },
+            //       {
+            //         loader: 'postcss-loader'
+            //       }
+            //     ]
+            // }
+            // {
+            //     test: /\.css$/,
+            //     use: ['style-loader', 'postcss-loader'],
+            // },
+            // {
+            //     test: /\.jsx?$/,
+            //     use: ['babel-loader', 'astroturf/loader'],
+            // },
         ]
-    }
+    },
+    // optimization: {
+    //     minimize: true,
+    //     minimizer: [
+    //       // For webpack@5 you can use the `...` syntax to extend existing minimizers (i.e. `terser-webpack-plugin`), uncomment the next line
+    //       // `...`
+    //       new CssMinimizerPlugin(),
+    //     ],
+    //   },
 };
