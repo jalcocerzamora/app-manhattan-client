2a3c2082d62c289ff0f4e01be4c3c87b77295085
JonAlco <alco_1993@hotmail.com>
2021-02-18 01:41:55 -0500

    dashboard

--- projects/dashboard/src/app/components/header/header.component.ts
+++ projects/dashboard/src/app/components/header/header.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    // do stuff
+  }
 
   ngOnInit(): void {
+    // do stuff
   }
 
 }
